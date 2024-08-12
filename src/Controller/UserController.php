28302f78b5b3047ddd32a39b1403bc4cb52398de
Sylvain Vandermeersch <sylvain.vandermeersch@gmail.com>
2024-08-13 01:42:14 +0200

    feat: add cache, add authorization,refacto entityManager by using method in Repo

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,27 +5,37 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
-
-#[Route('/users')]
+#[Route('/users',name: 'user_')]
+#[IsGranted(new Expression('is_granted("ROLE_ADMIN") or is_granted("ROLE_SUPER_ADMIN")'))]
 class UserController extends AbstractController
 {
     #[Route('/', name: 'user_list', methods: ['GET'])]
-    public function listAction(UserRepository $userRepository): Response
+    public function list(UserRepository $userRepository, TagAwareCacheInterface $cachePool): Response
     {
+        $idCache = 'getUsersList';
+        $usersList = $cachePool->get($idCache, function (ItemInterface $item) use ($userRepository) {
+            $item->tag('usersCache');
+            $cachedUsersList = $userRepository->findAll();
+
+            return $cachedUsersList;
+        });
         return $this->render('user/list.html.twig', [
-            'users' => $userRepository->findAll(),
+            'users' => $usersList,
         ]);
     }
 
-    #[Route('/create', name: 'user_create', methods: ['GET', 'POST'])]
-    public function createAction(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
+    #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
+    public function create(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $userPasswordHasher, TagAwareCacheInterface $cachePool): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
@@ -34,9 +44,8 @@ class UserController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
 
             $user->setPassword($userPasswordHasher->hashPassword($user, $user->getPassword()));
-            $entityManager->persist($user);
-            $entityManager->flush();
-
+            $userRepository->save($user, true);
+            $cachePool->invalidateTags(['usersCache']);
             $this->addFlash('success', "L'utilisateur a bien été ajouté.");
 
             return $this->redirectToRoute('user_list', [], Response::HTTP_SEE_OTHER);
@@ -48,15 +57,17 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'user_edit', methods: ['GET', 'POST'])]
-    public function editAction(Request $request, User $user, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, UserRepository $userRepository, UserPasswordHasherInterface $userPasswordHasher, TagAwareCacheInterface $cachePool): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $user->setPassword($userPasswordHasher->hashPassword($user, $user->getPassword()));
-            $entityManager->flush();
+            $userRepository->save($user, true);
+            $cachePool->invalidateTags(['usersCache']);
+            $this->addFlash('success', "L'utilisateur a bien été modifié.");
 
             return $this->redirectToRoute('user_list', [], Response::HTTP_SEE_OTHER);
         }
