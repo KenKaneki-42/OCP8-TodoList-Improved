c0046c5c099e70ae986722eec318480c08d6aaba
Sylvain Vandermeersch <sylvain.vandermeersch@gmail.com>
2024-08-19 16:01:37 +0200

    feat: add fixtures

---
+++ src/DataFixtures/TaskFixtures.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TaskFixtures extends Fixture
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Create a task
+        $task = new Task();
+        $task->setTitle('Title')
+              ->setContent('Content of the task')
+              ->setCreatedAt(new \DateTimeImmutable('now'));
+
+        $manager->persist($task);
+
+        $manager->flush();
+    }
+}
